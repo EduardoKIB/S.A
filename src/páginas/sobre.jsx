7e40fe40a3c23aaece364b8ac115ae60aaf72562
rojerio <renan_jussiani@estudante.sesisenai.org.br>
2024-09-03 09:37:07 -0300

    add img logo

--- src/páginas/sobre.jsx
+++ src/páginas/sobre.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import logo from "../img/logo.jpg"
+import Logo from '../img/logo.png'; // Assuming this path is correct
 
 function Sobre() {
-  return (
-    <>
-    <h1>olá</h1>
-    <img src={logo} alt="logo" />
-    </>
-  )
+    return (
+      <>
+        <img src={Logo} alt="Logo" style={{ width: '100px', height: 'auto' }} />
+        <h1>Sobre</h1>
+        <p>
+          A pousada Quinta do Ypuã oferece ao seus clientes um recanto de aconchego e lazer, em ambiente rústico e agradável. Ideal para quem gosta de fugir da rotina e procura um local de paz para descansar e curtir a natureza.
+          "O Ypuã tem tudo a ver com a natureza, dá para sentir a energia do lugar. Eu me preocupo se você vai comer bem, dormir bem e se vai se sentir em casa. Vou te mostrar onde encontrar os melhores frutos do mar, onde curtir a melhor praia e as melhores ondas. Mas se você não quiser fazer nada eu também conheço o melhor lugar."
+          — Heitor, Anfitrião da pousada
+        </p>
+      </>
+    );
 }
 
-export default Sobre
+export default Sobre;
