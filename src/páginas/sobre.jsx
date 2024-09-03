f627ec56a27f5b20a0f5640e26401a467f0abfdd
rojerio <renan_jussiani@estudante.sesisenai.org.br>
2024-09-03 09:12:05 -0300

    ajuste de img

--- src/páginas/sobre.jsx
+++ src/páginas/sobre.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from "../img/logo.jpg"
 
 function Sobre() {
   return (
     <>
     <h1>olá</h1>
-    <img src="https://static.wixstatic.com/media/b87f83_9f4625b043a944daaf5fddefc7d73d0e~mv2.png/v1/fill/w_80,h_80,al_c,q_85,enc_auto/logo-pousada-quinta-do-ypua.png" alt="img" />
+    <img src={logo} alt="logo" />
     </>
   )
 }
